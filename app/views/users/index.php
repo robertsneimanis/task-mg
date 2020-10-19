e037db27bef9c806a5b8e1633bed5c9d4961e2ab
roberts <naimanis.roberts@gmail.com>
2020-10-19 17:26:02 +0100

    changed php echo to =

--- app/views/users/index.php
+++ app/views/users/index.php
@@ -2,48 +2,48 @@
 
 <?php flash('register_success'); ?>
 
-<form action="<?php echo URLROOT; ?>/users/index" method="post">
+<form action="<?= URLROOT; ?>/users/index" method="post">
 
     <label for="email">Email: <sup>*</sup></label>
     <input type="email" name="email"
-        class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>"
-        value="<?php echo $data['email']; ?>">
-    <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
+        class="form-control form-control-lg <?= (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>"
+        value="<?= $data['email']; ?>">
+    <span class="invalid-feedback"><?= $data['email_err']; ?></span>
 
     <label for="password">Password: <sup>*</sup></label>
     <input type="password" name="password"
-        class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>"
-        value="<?php echo $data['password']; ?>">
-    <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
+        class="form-control form-control-lg <?= (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>"
+        value="<?= $data['password']; ?>">
+    <span class="invalid-feedback"><?= $data['password_err']; ?></span>
 
     <button type="submit" name="login" class="btn btn-success btn-block">Login</button>
 
 </form>
 
-<form action="<?php echo URLROOT; ?>/users/index" method="post">
+<form action="<?= URLROOT; ?>/users/index" method="post">
     <label for="name">Name: <sup>*</sup></label>
     <input type="text" name="register-name"
-        class="form-control form-control-lg <?php echo (!empty($data['register-name_err'])) ? 'is-invalid' : ''; ?>"
-        value="<?php echo $data['register-name']; ?>">
-    <span class="invalid-feedback"><?php echo $data['register-name_err']; ?></span>
+        class="form-control form-control-lg <?= (!empty($data['register-name_err'])) ? 'is-invalid' : ''; ?>"
+        value="<?= $data['register-name']; ?>">
+    <span class="invalid-feedback"><?= $data['register-name_err']; ?></span>
 
     <label for="email">Email: <sup>*</sup></label>
     <input type="email" name="register-email"
-        class="form-control form-control-lg <?php echo (!empty($data['register-email_err'])) ? 'is-invalid' : ''; ?>"
-        value="<?php echo $data['register-email']; ?>">
-    <span class="invalid-feedback"><?php echo $data['register-email_err']; ?></span>
+        class="form-control form-control-lg <?= (!empty($data['register-email_err'])) ? 'is-invalid' : ''; ?>"
+        value="<?= $data['register-email']; ?>">
+    <span class="invalid-feedback"><?= $data['register-email_err']; ?></span>
 
     <label for="password">Password: <sup>*</sup></label>
     <input type="password" name="register-password"
-        class="form-control form-control-lg <?php echo (!empty($data['register-password_err'])) ? 'is-invalid' : ''; ?>"
-        value="<?php echo $data['register-password']; ?>">
-    <span class="invalid-feedback"><?php echo $data['register-password_err']; ?></span>
+        class="form-control form-control-lg <?= (!empty($data['register-password_err'])) ? 'is-invalid' : ''; ?>"
+        value="<?= $data['register-password']; ?>">
+    <span class="invalid-feedback"><?= $data['register-password_err']; ?></span>
 
     <label for="confirm_password">Confirm Password: <sup>*</sup></label>
     <input type="password" name="register-confirm_password"
-        class="form-control form-control-lg <?php echo (!empty($data['register-confirm_password_err'])) ? 'is-invalid' : ''; ?>"
-        value="<?php echo $data['register-confirm_password']; ?>">
-    <span class="invalid-feedback"><?php echo $data['register-confirm_password_err']; ?></span>
+        class="form-control form-control-lg <?= (!empty($data['register-confirm_password_err'])) ? 'is-invalid' : ''; ?>"
+        value="<?= $data['register-confirm_password']; ?>">
+    <span class="invalid-feedback"><?= $data['register-confirm_password_err']; ?></span>
 
     <button type="submit" name="register" class="btn btn-success btn-block">Register</button>
 </form>
--- app/views/users/register.php
+++ app/views/users/register.php
@@ -4,26 +4,26 @@
       <div class="card card-body bg-light mt-5">
         <h2>Create An Account</h2>
         <p>Please fill out this form to register with us</p>
-        <form action="<?php echo URLROOT; ?>/users/register" method="post">
+        <form action="<?= URLROOT; ?>/users/register" method="post">
           <div class="form-group">
             <label for="name">Name: <sup>*</sup></label>
-            <input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['name']; ?>">
-            <span class="invalid-feedback"><?php echo $data['name_err']; ?></span>
+            <input type="text" name="name" class="form-control form-control-lg <?= (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" value="<?= $data['name']; ?>">
+            <span class="invalid-feedback"><?= $data['name_err']; ?></span>
           </div>
           <div class="form-group">
             <label for="email">Email: <sup>*</sup></label>
-            <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
-            <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
+            <input type="email" name="email" class="form-control form-control-lg <?= (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?= $data['email']; ?>">
+            <span class="invalid-feedback"><?= $data['email_err']; ?></span>
           </div>
           <div class="form-group">
             <label for="password">Password: <sup>*</sup></label>
-            <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>">
-            <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
+            <input type="password" name="password" class="form-control form-control-lg <?= (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?= $data['password']; ?>">
+            <span class="invalid-feedback"><?= $data['password_err']; ?></span>
           </div>
           <div class="form-group">
             <label for="confirm_password">Confirm Password: <sup>*</sup></label>
-            <input type="password" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['confirm_password']; ?>">
-            <span class="invalid-feedback"><?php echo $data['confirm_password_err']; ?></span>
+            <input type="password" name="confirm_password" class="form-control form-control-lg <?= (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" value="<?= $data['confirm_password']; ?>">
+            <span class="invalid-feedback"><?= $data['confirm_password_err']; ?></span>
           </div>
 
           <div class="row">
@@ -31,7 +31,7 @@
               <input type="submit" value="Register" class="btn btn-success btn-block">
             </div>
             <div class="col">
-              <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-light btn-block">Have an account? Login</a>
+              <a href="<?= URLROOT; ?>/users/login" class="btn btn-light btn-block">Have an account? Login</a>
             </div>
           </div>
         </form>
