c13421f9eccb3402476330fa0969447a90831a0e
roberts <naimanis.roberts@gmail.com>
2020-10-20 06:00:27 +0100

    changed table

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,8 @@
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
   define('DB_PASS', '123456');
-  define('DB_NAME', 'shareposts');
+  // define('DB_NAME', 'shareposts');
+  define('DB_NAME', 'magebit');
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
--- app/models/Post.php
+++ app/models/Post.php
@@ -7,7 +7,7 @@
     }
 
     public function getPosts($user_id){
-      $this->db->query('SELECT * FROM posts WHERE user_id = :user_id');
+      $this->db->query('SELECT * FROM attributes WHERE user_id = :user_id');
       $this->db->bind(':user_id', $user_id);
 
       $results = $this->db->resultSet();
@@ -15,25 +15,8 @@
       return $results;
     }
 
-    // public function getPosts(){
-    //   $this->db->query('SELECT *,
-    //                     posts.id as postId,
-    //                     users.id as userId,
-    //                     posts.created_at as postCreated,
-    //                     users.created_at as userCreated
-    //                     FROM posts
-    //                     INNER JOIN users
-    //                     ON posts.user_id = users.id
-    //                     ORDER BY posts.created_at DESC
-    //                     ');
-
-    //   $results = $this->db->resultSet();
-
-    //   return $results;
-    // }
-
     public function addPost($data){
-      $this->db->query('INSERT INTO posts (title, user_id, body) VALUES(:title, :user_id, :body)');
+      $this->db->query('INSERT INTO attributes (title, user_id, body) VALUES(:title, :user_id, :body)');
       // Bind values
       $this->db->bind(':title', $data['title']);
       $this->db->bind(':user_id', $data['user_id']);
@@ -48,7 +31,7 @@
     }
 
     public function updatePost($data){
-      $this->db->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
+      $this->db->query('UPDATE attributes SET title = :title, body = :body WHERE id = :id');
       // Bind values
       $this->db->bind(':id', $data['id']);
       $this->db->bind(':title', $data['title']);
@@ -63,7 +46,7 @@
     }
 
     public function getPostById($id){
-      $this->db->query('SELECT * FROM posts WHERE id = :id');
+      $this->db->query('SELECT * FROM attributes WHERE id = :id');
       $this->db->bind(':id', $id);
 
       $row = $this->db->single();
@@ -72,7 +55,7 @@
     }
 
     public function deletePost($id){
-      $this->db->query('DELETE FROM posts WHERE id = :id');
+      $this->db->query('DELETE FROM attributes WHERE id = :id');
       // Bind values
       $this->db->bind(':id', $id);
 
