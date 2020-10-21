0bc62923c8bf49b6e4d7417dbe62469d09dc0cd2
roberts <naimanis.roberts@gmail.com>
2020-10-21 17:24:53 +0100

    renamed post to attribute

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -170,7 +170,7 @@
       $_SESSION['user_id'] = $user->id;
       $_SESSION['user_email'] = $user->email;
       $_SESSION['user_name'] = $user->name;
-      redirect('posts');
+      redirect('attributes');
     }
 
     public function logout(){
--- app/views/attributes/add.php
+++ app/views/attributes/add.php
@@ -1,9 +1,9 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-  <a href="<?= URLROOT; ?>/posts" class="btn btn-light"><i class="fa fa-backward"></i> Back</a>
+  <a href="<?= URLROOT; ?>/attributes" class="btn btn-light"><i class="fa fa-backward"></i> Back</a>
   <div class="card card-body bg-light mt-5">
-    <h2>Add Post</h2>
-    <p>Create a post with this form</p>
-    <form action="<?= URLROOT; ?>/posts/add" method="post">
+    <h2>Add Attribute</h2>
+    <p>Create a attribute with this form</p>
+    <form action="<?= URLROOT; ?>/attributes/add" method="post">
       <div class="form-group">
         <label for="title">Title: <sup>*</sup></label>
         <input type="text" name="title" class="form-control form-control-lg <?= (!empty($data['title_err'])) ? 'is-invalid' : ''; ?>" value="<?= $data['title']; ?>">
